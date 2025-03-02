29275852fafb0b8e5a1d3fc3308391c549a7440f
yash agrawal <yashagrawaltech24@gmail.com>
2025-03-02 19:37:08 +0530

    added static file middleware

--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,10 @@
 import { config } from "dotenv";
 config();
 import path from "path"
+import { fileURLToPath } from 'url';
+import { dirname } from 'path';
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
 import express from "express";
 import cors from "cors";
 import cookieParser from "cookie-parser";
