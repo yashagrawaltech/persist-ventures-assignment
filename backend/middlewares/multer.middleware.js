fc4e33ee4b9c5d699591bf58ca1e7c85605f766e
yash agrawal <yashagrawaltech24@gmail.com>
2025-03-02 19:20:26 +0530

    multer bug fix

--- backend/middlewares/multer.middleware.js
+++ backend/middlewares/multer.middleware.js
@@ -3,7 +3,7 @@ import path from "path"
 
 const storage = multer.diskStorage({
   destination: function (req, file, cb) {
-    cb(null, "./public/temp");
+    cb(null, "../public/temp");
   },
   
   filename: function (req, file, cb) {
